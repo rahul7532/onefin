8b0130bdcae7e434b75d2797c21bef3eab1f7594
rahul verma <rahul.verma@zerovir.in>
2021-05-15 10:17:19 +0530

    Procfile

--- e2e/server.js
+++ e2e/server.js
@@ -10,4 +10,7 @@ app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/onefin/'}),
 );
 
-app.listen(process.env.PORT || 8080);
+let port=process.env.PORT || 8080;
+app.listen(port,()=>{
+    console.log(`App is running at the port ${port}}`)
+});
